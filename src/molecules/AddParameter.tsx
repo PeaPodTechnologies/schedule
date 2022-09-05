4c8ace281dacc7abf951b869629bdf559ea931f2
liamm91 <liam.major1@ontariotechu.net>
2022-09-05 16:18:20 -0400

    updated docstring

--- src/molecules/AddParameter.tsx
+++ src/molecules/AddParameter.tsx
@@ -8,7 +8,8 @@ type AddParameterProps = {
  * this object's purpose is to add another entry into the ScheduleBuilder's
  * ParameterBlock array.
  * 
- * this takes a function which will add a new ParameterBlock into the parent object.
+ * this object is stateless and has one purpose, its function is to 
+ * add a new ParameterBlock into the parent object.
  * in this case, it's the ScheduleBuilder
  */
 const AddParameter: FC<AddParameterProps> = props => {
