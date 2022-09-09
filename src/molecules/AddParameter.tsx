1266b13d310fdc065e2ce9018c793d05fe9e1d47
liamm91 <liam.major1@ontariotechu.net>
2022-09-09 00:54:45 -0400

    inlined button

--- src/molecules/AddParameter.tsx
+++ src/molecules/AddParameter.tsx
@@ -7,22 +7,20 @@ type AddParameterProps = {
 /**
  * this object's purpose is to add another entry into the ScheduleBuilder's
  * ParameterBlock array.
- * 
- * this object is stateless and has one purpose, its function is to 
+ *
+ * this object is stateless and has one purpose, its function is to
  * add a new ParameterBlock into the parent object.
  * in this case, it's the ScheduleBuilder
  */
 const AddParameter: FC<AddParameterProps> = props => {
 	return (
-		<>
-			<button
-				onClick={() => {
-					props.addParameter();
-				}}
-			>
-				Add a new parameter
-			</button>
-		</>
+		<button
+			onClick={() => {
+				props.addParameter();
+			}}
+		>
+			Add a new parameter
+		</button>
 	);
 };
 
