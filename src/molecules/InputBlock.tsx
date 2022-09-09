905cf2ba0359b5c8de7abc1bae8af2d6ee4d80f7
liamm91 <liam.major1@ontariotechu.net>
2022-09-09 00:57:00 -0400

    added optional fields

--- src/molecules/InputBlock.tsx
+++ src/molecules/InputBlock.tsx
@@ -3,12 +3,20 @@ import { capitalCase } from '../utils';
 
 interface InputBlockProps {
 	label: string;
+	value?: string;
+	readonly?: boolean;
 }
 
 const InputBlock: FC<InputBlockProps> = props => (
 	<div>
 		<label htmlFor={props.label}>{capitalCase(props.label) + ': '}</label>
-		<input type="text" name={props.label} id={props.label} />
+		<input
+			readOnly={props.readonly}
+			type="text"
+			name={props.label}
+			id={props.label}
+			value={props.value}
+		/>
 	</div>
 );
 
