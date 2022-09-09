39719c4c2eb4ddbf815c0360690a8796394d37f7
liamm91 <liam.major1@ontariotechu.net>
2022-09-09 00:57:45 -0400

    created a new block to reflect the UML and data

---
+++ src/molecules/PhaseBlock.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import InputBlock from './InputBlock';
+import { ValidFields } from "./ParameterBlock"
+
+/**
+ * these are the expected props that can be passed into the object
+ */
+interface PhaseBlockProps {
+	type: string;
+    end: number;
+    targets?: {
+        value: number;
+        timestamp?: number;
+        duration?: number
+    }[]
+    // a generic function which we will expand on within the component
+    onUpdate(field: ValidFields, value: any): void
+}
+
+const PhaseBlock: FC<PhaseBlockProps> = props => {
+    const updateType = (value: string) => {
+        return props.onUpdate("type", value);
+    }
+
+    const updateEnd = (value: number) => {
+        return props.onUpdate("end", value);
+    }
+
+	return <div>
+        <InputBlock label="type" onBlur={updateType}/>
+
+        <InputBlock label="end" onBlur={updateEnd}/>
+    </div>;
+};
+
+export default PhaseBlock;
