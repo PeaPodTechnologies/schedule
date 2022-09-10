9d8ce34f9bcc178c5b4a3d8b1eeea774c30b6eb5
Liam Major <liam.major1@ontariotechu.net>
2022-09-09 23:58:29 -0400

    phaseblock renders a targetblock per target - updated callbacks - updated typing of functions

--- src/molecules/PhaseBlock.tsx
+++ src/molecules/PhaseBlock.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
+import { underscoreJoin } from '../utils';
 import InputBlock from './InputBlock';
-import { ValidFields } from "./ParameterBlock"
+import { PhaseTypes, ValidFields } from './ParameterBlock';
+import TargetBlock from './TargetBlock';
 
 /**
  * these are the expected props that can be passed into the object
  */
 interface PhaseBlockProps {
-	type: string;
+	type: PhaseTypes;
     end: number;
     targets?: {
         value: number;
@@ -14,7 +16,11 @@ interface PhaseBlockProps {
         duration?: number
     }[]
     // a generic function which we will expand on within the component
-    onUpdate(field: ValidFields, value: any): void
+	onUpdate(field: ValidFields, value: any): void;
+	// passing in a callback for the target to update itself
+	updateTarget(targetIndex: number, field: any, value: number): void;
+	// callback to delete a target from the phase
+	deleteTarget(targetIndex: number): void;
 }
 
 const PhaseBlock: FC<PhaseBlockProps> = props => {
@@ -26,11 +32,41 @@ const PhaseBlock: FC<PhaseBlockProps> = props => {
         return props.onUpdate("end", value);
     }
 
-	return <div>
+	// rendering
+	return (
+		<>
+			<div>
         <InputBlock label="type" onBlur={updateType} value={props.type} />
-
         <InputBlock label="end" onBlur={updateEnd} value={props.end} />
-    </div>;
+			</div>
+			{props.targets == undefined ? (
+				<></>
+			) : (
+				<>
+					<ol start={1}>
+						{props.targets.map((target, index) => {
+							return (
+								<li key={underscoreJoin('target', index)}>
+									<TargetBlock
+										timestamp={target.timestamp}
+										duration={target.duration}
+										type={props.type}
+										value={target.value}
+										onUpdate={(field, value) => {
+											props.updateTarget(index, field, value);
+										}}
+										delete={() => {
+											props.deleteTarget(index);
+										}}
+									/>
+								</li>
+							);
+						})}
+					</ol>
+				</>
+			)}
+		</>
+	);
 };
 
 export default PhaseBlock;
