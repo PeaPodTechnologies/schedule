4efb6fe21b2949f72ddbaf51ba32a1277d66f13e
Liam Major <liam.major1@ontariotechu.net>
2022-09-09 23:58:29 -0400

    added a function to properly set the label text

--- src/molecules/InputBlock.tsx
+++ src/molecules/InputBlock.tsx
@@ -8,9 +8,23 @@ interface InputBlockProps {
 	readonly?: boolean;
 }
 
+const sanitizeInput = (input: string) => {
+	// getting the last two chars of the input
+	let trailing = input.substring(input.length - 2);
+
+	// testing if there is a semicolon at the end of the string
+	if (trailing.at(0) != ':') input += ':';
+
+	// testing if there is a space at the end of the string
+	if (trailing.at(1) != ' ') input += ' ';
+
+	// returning the output
+	return capitalCase(input);
+};
+
 const InputBlock: FC<InputBlockProps> = props => (
 	<div>
-		<label htmlFor={props.label}>{capitalCase(props.label) + ': '}</label>
+		<label htmlFor={props.label}>{sanitizeInput(props.label)}</label>
 		<input
 			readOnly={props.readonly}
 			type="text"
