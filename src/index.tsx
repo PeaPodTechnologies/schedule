d1aeee93969c12c601647d656ba46b172d675de0
Liam Major <liam.major1@ontariotechu.net>
2022-09-10 19:57:55 -0400

    removed react.strictmode - strictmode caused hooks to execute twice performing twice the amount of operations

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ScheduleBuilder from './pages/ScheduleBuilder';
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<React.StrictMode>
-		<ScheduleBuilder />
-	</React.StrictMode>
+	<ScheduleBuilder />
 );
 
 reportWebVitals();
