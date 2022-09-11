f27f5ecbc1efb8a074bee04b8dfab66895d3bf6c
Liam Major <liam.major1@ontariotechu.net>
2022-09-10 20:40:30 -0400

    removed unused function

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,13 +1,4 @@
 /**
- * this function is meant to create a random number which will be used for IDs
- * @param limit the largest number this function can generate
- * @returns a random number between 0 and the limit
- */
-export function createRandomId(limit: number = 65536) {
-	return (Math.random() * limit).toFixed();
-}
-
-/**
  * this function capitalizes the first letter in the string
  * @param str the string to capitalize
  */
