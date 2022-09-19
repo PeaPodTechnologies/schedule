8c891a30b35d4784565bffa840ccb35a2589f32b
Liam Major <liam.major1@ontariotechu.net>
2022-09-19 11:25:00 -0400

    added selected tag - pretty printed

--- src/atoms/SelectBlock.tsx
+++ src/atoms/SelectBlock.tsx
@@ -1,23 +1,34 @@
 import { FC } from 'react';
-import { v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { toCapitalCase, toLabel } from '../utils';
 
-interface SelectBlockProps {
+export interface SelectBlockProps {
 	label: string;
 	onChange(value: string): void;
 	options: { [key: string]: string };
-	text: string;
+	selected: string;
 }
 
-const SelectBlock: FC<SelectBlockProps> = props => (
-	<div>
-		<label htmlFor={props.label}>{toLabel(props.text)}</label>
-		<select onChange={event => props.onChange(event.target.value)} name={props.label}>
-			{Object.entries(props.options).map(([key, value]) => {
-				return <option key={uuid()} value={value}>{toCapitalCase(key)}</option>;
-			})}
-		</select>
-	</div>
-);
+const SelectBlock: FC<SelectBlockProps> = props => {
+	return (
+		<div>
+			<label htmlFor={props.label}>{toLabel(props.label)}</label>
+			<select
+				value={props.selected}
+				onChange={event => props.onChange(event.target.value)}
+				name={props.label}
+			>
+				{Object.entries(props.options).map(([key, value]) => {
+					return (
+						// need to choose the one that's selected
+						<option key={uuid()} value={value}>
+							{toCapitalCase(key)}
+						</option>
+					);
+				})}
+			</select>
+		</div>
+	);
+};
 
 export default SelectBlock;
