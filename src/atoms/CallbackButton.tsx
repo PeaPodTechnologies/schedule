1771a3ecbd3d9b140be291adf924fa27c5c65d63
Liam Major <liam.major1@ontariotechu.net>
2022-09-21 02:10:15 -0400

    added a disabled prop - pretty printed

--- src/atoms/CallbackButton.tsx
+++ src/atoms/CallbackButton.tsx
@@ -4,23 +4,27 @@ import { Button } from '@mui/material';
 interface CallbackButtonProps {
 	callback(): void;
 	text?: string;
-    startIcon?: JSX.Element;
-    endIcon?: JSX.Element;
+	startIcon?: JSX.Element;
+	endIcon?: JSX.Element;
+	disabled?: boolean;
 }
 
-const CallbackButton: FC<CallbackButtonProps> = props => (
-	<div>
-		<Button
-			variant="outlined"
-			onClick={() => {
-				props.callback();
-			}}
-            startIcon={props.startIcon}
-            endIcon={props.endIcon}
-		>
-			{props.text}
-		</Button>
-	</div>
-);
+const CallbackButton: FC<CallbackButtonProps> = props => {
+	return (
+		<div>
+			<Button
+				disabled={props.disabled}
+				variant="outlined"
+				onClick={() => {
+					props.callback();
+				}}
+				startIcon={props.startIcon}
+				endIcon={props.endIcon}
+			>
+				{props.text}
+			</Button>
+		</div>
+	);
+};
 
 export default CallbackButton;
